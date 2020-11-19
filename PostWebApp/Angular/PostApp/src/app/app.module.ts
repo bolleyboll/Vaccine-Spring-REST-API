437593365d5ce16c4d4e0f3f4900045b788ff384
Aman Gupta <aman.iv0012@gmail.com>
2020-11-19 13:02:58 +0530

    :bug: Fixed Imports

--- PostWebApp/Angular/PostApp/src/app/app.module.ts
+++ PostWebApp/Angular/PostApp/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AddComponent } from './add/add.component';
@@ -8,7 +9,7 @@ import { ViewComponent } from './view/view.component';
 
 @NgModule({
   declarations: [AppComponent, AddComponent, UpdateComponent, ViewComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
