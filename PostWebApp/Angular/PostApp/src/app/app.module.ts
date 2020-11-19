c537eb7a4a63262b1281d5e976c87f762dc0bca5
mumteshgarg <gargmumtesh97@gmail.com>
2020-11-19 14:31:09 +0530

    Routes added

--- PostWebApp/Angular/PostApp/src/app/app.module.ts
+++ PostWebApp/Angular/PostApp/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { AddComponent } from './add/add.component';
 import { UpdateComponent } from './update/update.component';
 import { ViewComponent } from './view/view.component';
 
+export const route: Routes = [
+  {
+    path: 'Update',
+    component: UpdateComponent
+  }
+]
+
 @NgModule({
   declarations: [AppComponent, AddComponent, UpdateComponent, ViewComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, RouterModule.forRoot(route)],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
