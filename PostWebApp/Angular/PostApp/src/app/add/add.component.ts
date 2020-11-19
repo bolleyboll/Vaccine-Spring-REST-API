d0f1f1b43342b7aec85bd51fa5e742c146eee8f7
Aman Gupta <aman.iv0012@gmail.com>
2020-11-19 12:12:43 +0530

    :100: Forms for Adding and Updating a Post

---
+++ PostWebApp/Angular/PostApp/src/app/add/add.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add',
+  templateUrl: './add.component.html',
+  styleUrls: ['./add.component.css'],
+})
+export class AddComponent {
+  constructor() {}
+}
--- PostWebApp/Angular/PostApp/src/app/app.module.ts
+++ PostWebApp/Angular/PostApp/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AddComponent } from './add/add.component';
+import { UpdateComponent } from './update/update.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddComponent,
+    UpdateComponent,
   ],
   imports: [
     BrowserModule
