6322060427a3bfeef644eab389c76da956e6bd0a
Nagveni Kapileshwari <74416829+nagveni-kapileshwari@users.noreply.github.com>
2020-11-23 14:44:28 +0530

    organization functions added in app controller

--- src/main/java/com/example/vaccine/AppController.java
+++ src/main/java/com/example/vaccine/AppController.java
@@ -1,10 +1,16 @@
 package com.example.vaccine;
 
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,18 +22,63 @@ public class AppController {
 
 	@Autowired
 	private OrganizationsRepository orgRepository;
+	
+	@Autowired
+	private VaccinesRepository vacRepository;
 
 	@Autowired
 	private HttpSession httpSession;
+	
+	public boolean orgValidate() {
+		return (httpSession.getAttribute("orgId") == null) ? false : true;
+	}
 
 	@PostMapping("/org/signup")
 	public Organization signUp(@RequestBody Organization org) {
+		try {
+			org = orgRepository.save(org);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			return null;
+		}
+		if (org.getOrgId() != 0) {
+			return org;
+		}
 		return null;
 	}
 
 	@PostMapping("/org/signin")
 	public Organization signIn(@RequestBody Organization org) {
-
+		Organization dbOrg = orgRepository.findByEmailAndPassword(org.getEmail(), org.getPassword());
+		if (dbOrg != null && dbOrg.getEmail().equals(org.getEmail())
+				&& dbOrg.getPassword().equals(org.getPassword())) {
+			httpSession.setAttribute("orgId", dbOrg.getOrgId());
+			return dbOrg;
+		}
 		return null;
+	}
+	
+	@GetMapping("/org")
+	public List<Organization> getOrganizations() {
+		return (orgValidate()) ? (List<Organization>) orgRepository.findAll() : null;
+	}
+
+	@PutMapping("/org/update")
+	public Organization updatePost(@RequestBody Organization org) {
+		return (orgValidate()) ? orgRepository.save(org) : null;
+	}
+	
+	@DeleteMapping("/org/delete/{id}")
+	public Status deletePost(@PathVariable Integer id) {
+		if (!orgValidate()) {
+			return null;
+		}
+		try {
+			orgRepository.deleteById(id);
+			return new Status(true);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			return new Status(false);
+		}
 	}
 }
--- src/main/java/com/example/vaccine/OrganizationsRepository.java
+++ src/main/java/com/example/vaccine/OrganizationsRepository.java
@@ -3,6 +3,6 @@ package com.example.vaccine;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OrganizationsRepository extends CrudRepository<Organization, Integer> {
-	
+	Organization findByEmailAndPassword(String email, String password);
 
 }
