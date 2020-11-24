9dec1564ef8449042da83f984e204ffe89c783c5
Aman Gupta <31176934+bolleyboll@users.noreply.github.com>
2020-11-24 20:48:03 +0530

    :100: GET w/o Login Done + Minor Naming Conventions Fixed

--- src/main/java/com/example/vaccine/AppController.java
+++ src/main/java/com/example/vaccine/AppController.java
@@ -22,29 +22,40 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class AppController {
 
 	@Autowired
-	private OrganizationsRepository orgRepository;
-	
+	private OrganizationRepository orgRepository;
+
 	@Autowired
-	private VaccinesRepository vacRepository;
-	
+	private VaccineRepository vacRepository;
+
 	@Autowired
 	private PatientRepository patientRepository;
-	
+
 	@Autowired
 	private ReportRepository reportRepository;
 
 	@Autowired
 	private HttpSession httpSession;
-	
+
 	public boolean orgValidate() {
 		return (httpSession.getAttribute("orgId") == null) ? false : true;
 	}
+
 	public boolean patientValidate() {
-		return (httpSession.getAttribute("patientId")==null) ? false : true;
-}
+		return (httpSession.getAttribute("patientId") == null) ? false : true;
+	}
+
+	@GetMapping("/partners")
+	public List<Organization> getAllOrganizations() {
+		return (List<Organization>) orgRepository.findAll();
+	}
+
+	@GetMapping("/vaccines")
+	public List<Vaccine> getAllVaccines() {
+		return (List<Vaccine>) vacRepository.findAll();
+	}
 
 	@PostMapping("/org/signup")
-	public Organization signUp(@RequestBody Organization org) {
+	public Organization orgSignUp(@RequestBody Organization org) {
 		try {
 			org = orgRepository.save(org);
 		} catch (Exception e) {
@@ -60,24 +71,18 @@ public class AppController {
 	@PostMapping("/org/signin")
 	public Organization signIn(@RequestBody Organization org) {
 		Organization dbOrg = orgRepository.findByEmailAndPassword(org.getEmail(), org.getPassword());
-		if (dbOrg != null && dbOrg.getEmail().equals(org.getEmail())
-				&& dbOrg.getPassword().equals(org.getPassword())) {
+		if (dbOrg != null && dbOrg.getEmail().equals(org.getEmail()) && dbOrg.getPassword().equals(org.getPassword())) {
 			httpSession.setAttribute("orgId", dbOrg.getOrgId());
 			return dbOrg;
 		}
 		return null;
-	}
-	
-	@GetMapping("/org")
-	public List<Organization> getOrganizations() {
-		return (orgValidate()) ? (List<Organization>) orgRepository.findAll() : null;
 	}
 
 	@PutMapping("/org/update")
 	public Organization updateOrganization(@RequestBody Organization org) {
 		return (orgValidate()) ? orgRepository.save(org) : null;
 	}
-	
+
 	@DeleteMapping("/org/delete/{id}")
 	public Status deleteOrganization(@PathVariable Integer id) {
 		if (!orgValidate()) {
@@ -91,7 +96,7 @@ public class AppController {
 			return new Status(false);
 		}
 	}
-	
+
 	@GetMapping("/org/profile/{id}")
 	public Optional<Organization> getOrgById(@PathVariable int id) {
 		return (orgValidate()) ? orgRepository.findById(id) : null;
@@ -125,7 +130,7 @@ public class AppController {
 			return new Status(false);
 		}
 	}
-	
+
 	@PostMapping("/pat/signup")
 	public Patient patSignUp(@RequestBody Patient patient) {
 		try {
@@ -134,31 +139,34 @@ public class AppController {
 			System.out.println(e.getMessage());
 			return null;
 		}
-		if(patient.getPatientId()!=0) {
+		if (patient.getPatientId() != 0) {
 			return patient;
 		}
-		
+
 		return null;
 	}
-	
+
 	@PostMapping("/pat/signin")
 	public Patient patSignIn(@RequestBody Patient patient) {
 		Patient dbPatient = patientRepository.findByEmailAndPassword(patient.getEmail(), patient.getPassword());
-		if(dbPatient!=null && dbPatient.getEmail().equals(patient.getEmail()) && dbPatient.getPassword().equals(patient.getPassword())) {
+		if (dbPatient != null && dbPatient.getEmail().equals(patient.getEmail())
+				&& dbPatient.getPassword().equals(patient.getPassword())) {
 			httpSession.setAttribute("patientId", dbPatient.getPatientId());
 			return dbPatient;
 		}
 		return null;
 	}
+
 	@GetMapping("/pat")
-	public List<Patient> getPatients(){
+	public List<Patient> getPatients() {
 		return (orgValidate()) ? (List<Patient>) patientRepository.findAll() : null;
 	}
+
 	@PutMapping("/pat/update")
 	public Patient updatePatient(@RequestBody Patient patient) {
 		return (patientValidate()) ? patientRepository.save(patient) : null;
 	}
-	
+
 	@DeleteMapping("/pat/delete/{id}")
 	public Status deletePatient(@PathVariable Integer id) {
 		if (!patientValidate()) {
@@ -172,22 +180,22 @@ public class AppController {
 			return new Status(false);
 		}
 	}
-	
-	
+
 	@PostMapping("/rep/add")
 	public Report addReport(@RequestBody Report report) {
-		
+
 		return (orgValidate()) ? reportRepository.save(report) : null;
 	}
-	
+
 	@PutMapping("/rep/update")
 	public Report updateReport(@RequestBody Report report) {
 		return (orgValidate()) ? reportRepository.save(report) : null;
-		
+
 	}
+
 	@DeleteMapping("/rep/delete/{id}")
 	public Status deleteReport(@PathVariable Integer id) {
-		if(!orgValidate()) {
+		if (!orgValidate()) {
 			return null;
 		}
 		try {
@@ -198,18 +206,10 @@ public class AppController {
 			return new Status(false);
 		}
 	}
-	
-	
+
 	@GetMapping("/pat/{id}/rep")
 	public List<Report> getReportByPatientId(@PathVariable int id) {
 		return (patientValidate()) ? reportRepository.findReportByPatientId(id) : null;
 	}
-	
-	
-	
-	
-	
-	
-	
-	
+
 }
--- src/main/java/com/example/vaccine/OrganizationsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.vaccine;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface OrganizationsRepository extends CrudRepository<Organization, Integer> {
-	Organization findByEmailAndPassword(String email, String password);
-
-}
