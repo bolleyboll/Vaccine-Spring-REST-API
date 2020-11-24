f8e7d397db4e372ffe71b270466a4d479f22f9aa
Aman Gupta <31176934+bolleyboll@users.noreply.github.com>
2020-11-24 20:49:12 +0530

    Fixing Last Commit

---
+++ src/main/java/com/example/vaccine/OrganizationRepository.java
@@ -0,0 +1,10 @@
+package com.example.vaccine;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface OrganizationRepository extends CrudRepository<Organization, Integer> {
+	Organization findByEmailAndPassword(String email, String password);
+
+	Organization findByOrgId(Integer orgId);
+
+}
