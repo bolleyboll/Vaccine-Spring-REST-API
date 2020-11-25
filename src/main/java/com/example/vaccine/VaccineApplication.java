4575c5f0722d4d167e60c25431e635b8c8cfeb16
mumteshgarg <gargmumtesh97@gmail.com>
2020-11-25 20:25:39 +0530

    bean added

--- src/main/java/com/example/vaccine/VaccineApplication.java
+++ src/main/java/com/example/vaccine/VaccineApplication.java
@@ -2,6 +2,10 @@ package com.example.vaccine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class VaccineApplication {
@@ -10,4 +14,20 @@ public class VaccineApplication {
 		SpringApplication.run(VaccineApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/patient/signin").allowedOrigins("http://localhost:4200");
+				registry.addMapping("/patient/signin").allowCredentials(true);
+				
+				registry.addMapping("/patient/signup").allowedOrigins("http://localhost:4200");
+				registry.addMapping("/patient/signup").allowCredentials(true);
+				
+				registry.addMapping("/patient/update").allowedOrigins("http://localhost:4200");
+				registry.addMapping("/patient/update").allowCredentials(true);
+			}
+		};
+	}
 }
