f7bcc2d66902279d22c0cd62743cde73a37f5973
madhurijk <madhurikulkarni609@gmail.com>
2020-11-25 20:56:47 +0530

    added cross link to 4200

--- src/main/java/com/example/vaccine/AppController.java
+++ src/main/java/com/example/vaccine/AppController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @RestController
-@CrossOrigin(origins = "*", methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT,
+@CrossOrigin(origins = "http://localhost:4200", methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT,
 		RequestMethod.DELETE })
 public class AppController {
 
